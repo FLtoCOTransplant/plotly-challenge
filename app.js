995863aca6c35e7a95e03ebdee0c92c0c4ef7c97
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2021-01-14 23:37:19 -0700

    Working on bubble chart

--- app.js
+++ app.js
@@ -15,12 +15,13 @@ d3.json('samples.json').then((samples)=>{
     })
 })
 
-// Create horizontal bar chart with a dropdown menu
+// Create define the source fo the data and make the charts
 function makePlot(testId){
     d3.json('samples.json').then((samples)=>{
         // create an array
         var samples=samples.samples;
         var testNum=samples.map(row=>row.id).indexOf(testId);
+
         // Define the bar chart
         var otuValueTen=samples.map(row=>row.sample_values);
         var otuValueTen=otuValueTen[testNum].slice(0,10).reverse();
@@ -35,3 +36,41 @@ function makePlot(testId){
             type:'bar',
             orientation:'h'
         }
+        Plotly.newPlot('bar',[trace]);
+
+        // Define the bubble chart
+        var otuValue=samples.map(row=>row.sample_values);
+        var otuValue=otuValue[testNum];
+        var otuId=samples.map(row=>row.otu_ids);
+        var otuId=otuId[testNum];
+        var otuLabel=samples.map(row=>row.otu_labels); 
+        var otuLabel=otuLabel[testNum];
+        var minIds=d3.min(otuId);
+        var maxIds=d3.max(otuId);
+        var mapNr = d3.scaleLinear().domain([minIds, maxIds]).range([0, 1]);
+        var bubbleColors = otuId.map( val => d3.interpolateRgbBasis(["royalblue", "greenyellow", "goldenrod"])(mapNr(val)));
+        var trace1={
+            x: otuId,
+            y: otuValue,
+            text: otuLabel,
+            mode: 'markers',
+            marker: {
+                color: bubbleColors,
+                size: otuValue.map(x=>x*10),
+                sizemode: 'area'
+            }
+        };
+        var data1=[trace1];
+        var bubbleLayout={
+            xaxis:{
+                autochange: true,
+                height: 600,
+                width: 1000,
+                title: {
+                    text: 'OTU ID'
+                }
+            },
+        };
+        Plotly.newPlot('bubble',data1,bubbleLayout); 
+
+        
